7f5835301edbfd3025b7ff5a7f6c0be99239cab0
priyesh <priyesh.potdar@journeylabs.io>
2018-11-17 10:10:41 +0530

    Initial Commit

---
+++ src/main/java/com/meetup/demo/helloworld/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.meetup.demo.helloworld;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHelloController {
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> sayHello() {
+		return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+	}
+	
+}
